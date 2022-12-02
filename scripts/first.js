33816a0969fdf5061e37d68aaf194062e85eb886
akashstyle <akash.darsigunta@gmail.com>
2022-12-03 00:21:50 +0530

    tasks

---
+++ scripts/first.js
@@ -0,0 +1,55 @@
+// mathematical operators
+let x = 5;
+let y = 7;
+let addition = x+y;
+let substraction = x-y;
+let multiplication = x*y;
+let division = x/y;
+let modulo = x%y;
+console.log(addition,substraction,multiplication,division,modulo);
+//logical operators
+//And operator
+let Number = 17;
+let number = 22;
+console.log(Number>number && Number<number);
+//OR operator
+console.log(Number>number|| Number<number);
+// NOT operator
+console.log(!(number>Number&& Number<number));
+//comparision operators
+console.log(x<y,x>y,x==y,x<=y,x>=y);
+//Ternary operator
+let result = (8>9)?"true":"false";
+console.log(result);
+// Increment and Decrement
+console.log(x++);
+console.log(++x);
+console.log(--x);
+console.log(x--);
+ console.log(x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
